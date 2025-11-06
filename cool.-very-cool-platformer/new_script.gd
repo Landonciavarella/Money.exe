480994dcbfbca9b5bde424494362fc667d856d82
Landonciavarella <landonciavarella@gmail.com>
2025-11-06 16:36:49 -0500

    ahhhhhhhh
    I am not the cat.

---
+++ cool.-very-cool-platformer/new_script.gd
@@ -0,0 +1,10 @@
+extends Sprite2D
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name != "player":
+		return
+	call_deferred("reload_current_scene")
+		
+func reload_current_scene():
+	get_tree().reload_current_scene()
